0cc35ba5df03cc00189b8e53a17f733e4d40a720
sakastudio <sakastudio100@gmail.com>
2021-10-26 22:34:47 +0900

    ブロック設置プロトコル

---
+++ Source/moorestech_client/BlockPlaceProtocol.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlockPlaceProtocol.h"
+
+
+void UBlockPlaceProtocol::BlockPlace(int blockPositionX,int blockPositionY,int blockID)
+{
+    UE_LOG(LogTemp, Log, TEXT("called!!!"));
+}
