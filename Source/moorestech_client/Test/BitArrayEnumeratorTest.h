608c9f0619a88562883589ca598d9f69901c1afb
sakastudio <sakastudio100@gmail.com>
2021-10-31 21:38:55 +0900

    unit testのクラス

---
+++ Source/moorestech_client/Test/BitArrayEnumeratorTest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FunctionalTest.h"
+#include "BitArrayEnumeratorTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOORESTECH_CLIENT_API ABitArrayEnumeratorTest : public AFunctionalTest
+{
+	GENERATED_BODY()
+	
+};
