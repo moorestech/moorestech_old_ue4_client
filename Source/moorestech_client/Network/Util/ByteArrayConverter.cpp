52243b6c6d1063b8e2c00767856e6abed364e29a
sakastudio <sakastudio100@gmail.com>
2021-10-31 22:39:39 +0900

    変換追加

---
+++ Source/moorestech_client/Network/Util/ByteArrayConverter.cpp
@@ -0,0 +1,47 @@
+﻿#include "ByteArrayConverter.h"
+
+static TArray<uint8> ToByteArray(int sendData)
+{
+	TArray<uint8> result;
+	for (int i = 0; i < 4; i++)
+	{
+		result.Add(sendData >> i * 8);
+	}
+	return result;
+}
+static TArray<uint8> ToByteArray(int16 sendData)
+{
+	TArray<uint8> result;
+	for (int i = 0; i < 2; i++)
+	{
+		result.Add(sendData >> i * 8);
+	}
+	return result;
+}
+static TArray<uint8> ToByteArray(float sendData)
+{
+	TArray<uint8> result;
+	unsigned char const * p = reinterpret_cast<unsigned char const *>(&sendData);
+	for (int i = 0; i != sizeof(float); i++)
+	{
+		result.Add((uint8)p[i]);
+	}
+	return result;
+}
+static TArray<uint8> ToByteArray(FString sendData)
+{
+	FTCHARToUTF8 Convert(*sendData);
+	int BytesLength = Convert.Length(); 
+	uint8* messageBytes = static_cast<uint8*>(FMemory::Malloc(BytesLength));
+	FMemory::Memcpy(messageBytes, (uint8*)TCHAR_TO_UTF8(sendData.GetCharArray().GetData()), BytesLength); 
+
+	TArray<uint8> result;
+	for (int i = 0; i < BytesLength; i++)
+	{
+		result.Add(messageBytes[i]);
+	}
+
+	FMemory::Free(messageBytes);	
+
+	return result;
+}
---
+++ Source/moorestech_client/Network/Util/ByteArrayConverter.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+class ByteArrayConverter
+{
+public:
+	static TArray<uint8> ToByteArray(int sendData);
+	static TArray<uint8> ToByteArray(int16 sendData);
+	static TArray<uint8> ToByteArray(float sendData);
+	static TArray<uint8> ToByteArray(FString sendData);
+};
