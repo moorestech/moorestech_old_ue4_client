ce4289def085eca463b7b744b8c4ebc30241f6d3
sakastudio <sakastudio100@gmail.com>
2021-11-03 19:22:55 +0900

    endian判定

---
+++ Source/moorestech_client/Network/Util/endian.cpp
@@ -0,0 +1,14 @@
+﻿#include "endian.h"
+
+bool endian::IS_LITTLE_ENDIAN()
+{
+	int x=1; // 0x00000001
+	if (*(char*)&x) {
+		/* little endian. memory image 01 00 00 00 */
+		return true;
+	}else{
+		/* big endian. memory image 00 00 00 01 */
+		return false;
+	}
+}
+
---
+++ Source/moorestech_client/Network/Util/endian.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class endian
+{
+public:
+	static bool IS_LITTLE_ENDIAN();
+};
