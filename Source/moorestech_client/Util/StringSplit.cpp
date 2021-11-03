6c74759706fcdb0067a6b024da374165643ce22e
sakastudio <sakastudio100@gmail.com>
2021-11-03 20:39:35 +0900

    stringのsplit

---
+++ Source/moorestech_client/Util/StringSplit.cpp
@@ -0,0 +1,21 @@
+﻿#include "StringSplit.h"
+#include "iostream"
+#include "string"
+#include "vector"
+
+TArray<FString> StringSplit::split(FString str, char del) {
+	TArray<FString> result;
+	std::string subStr;
+ 
+	for (const char c : str) {
+		if (c == del) {
+			result.Add(subStr.c_str());
+			subStr.clear();
+		}else {
+			subStr += c;
+		}
+	}
+ 
+	result.Add(subStr.c_str());
+	return result;
+}
---
+++ Source/moorestech_client/Util/StringSplit.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "iostream"
+#include "string"
+#include "vector"
+
+class StringSplit
+{
+public:
+	static TArray<FString> split(FString str, char del);
+};
