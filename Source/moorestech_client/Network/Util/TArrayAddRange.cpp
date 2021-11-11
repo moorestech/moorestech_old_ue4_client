144aaf0ed8edb6b7dfe74558845de0662bd68d9d
sakastudio <sakastudio100@gmail.com>
2021-11-12 00:44:00 +0900

    AddRange用スクリプト

---
+++ Source/moorestech_client/Network/Util/TArrayAddRange.cpp
@@ -0,0 +1,26 @@
+﻿#include "TArrayAddRange.h"
+#include "moorestech_client/Network/Util/ByteArrayConverter.h"
+
+void TArrayAddRange::AddRange(TArray<uint8>& ByteArray, TArray<uint8>& Range)
+{
+    for (int32 i = 0; i < Range.Num(); i++)
+    {
+        ByteArray.Add(Range[i]);
+    }
+}
+void TArrayAddRange::AddRange(TArray<uint8>& ByteArray, int data)
+{
+    AddRange(ByteArray,ByteArrayConverter::ToByteArray(data));
+}
+void TArrayAddRange::AddRange(TArray<uint8>& ByteArray, int16 data)
+{
+    AddRange(ByteArray,ByteArrayConverter::ToByteArray(data));
+}
+void TArrayAddRange::AddRange(TArray<uint8>& ByteArray, float data)
+{
+    AddRange(ByteArray,ByteArrayConverter::ToByteArray(data));
+}
+void TArrayAddRange::AddRange(TArray<uint8>& ByteArray, FString& data)
+{
+    AddRange(ByteArray,ByteArrayConverter::ToByteArray(data));
+}
---
+++ Source/moorestech_client/Network/Util/TArrayAddRange.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+class TArrayAddRange
+{
+public:
+	static void AddRange(TArray<uint8>& ByteArray, TArray<uint8>& Range);
+	static void AddRange(TArray<uint8>& ByteArray, int data);
+	static void AddRange(TArray<uint8>& ByteArray, int16 data);
+	static void AddRange(TArray<uint8>& ByteArray, float data);
+	static void AddRange(TArray<uint8>& ByteArray, FString& data);
+};
