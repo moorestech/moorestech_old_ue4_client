ca7f709b3a6ac8446d65bc0a43fbb002de14bfb6
sakastudio <sakastudio100@gmail.com>
2021-11-11 22:37:37 +0900

    新しいコネクション用uasset

---
+++ Source/moorestech_client/Network/Recive/moorestechSocketConnection.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TcpSocketConnection.h"
+#include "moorestechSocketConnection.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOORESTECH_CLIENT_API AmoorestechSocketConnection : public ATcpSocketConnection
+{
+	GENERATED_BODY()
+	
+};
