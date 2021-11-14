1095c62dbdde9537b89db39f0dedeb27e710e483
sakastudio <sakastudio100@gmail.com>
2021-11-14 22:54:07 +0900

    イベントのレスポンスを受け渡すクラス

---
+++ Source/moorestech_client/Network/Recive/PacketAnalysis/EventResponseAnalysis.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EventResponseAnalysis.h"
+
+
+// Sets default values
+AEventResponseAnalysis::AEventResponseAnalysis()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AEventResponseAnalysis::Analysis(BitArrayEnumerator* BitArrayEnumerator)
+{
+	short i = BitArrayEnumerator->MoveNextToShort();
+	if(i < EventPacketAnalysisBases.Num())
+	{
+		EventPacketAnalysisBases[i]->Analysis(BitArrayEnumerator);
+	}
+}
+
---
+++ Source/moorestech_client/Network/Recive/PacketAnalysis/EventResponseAnalysis.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PacketAnalysisBase.h"
+#include "GameFramework/Actor.h"
+#include "moorestech_client/Network/Recive/EventAnalysis/EventPacketAnalysisBase.h"
+#include "EventResponseAnalysis.generated.h"
+
+UCLASS()
+class MOORESTECH_CLIENT_API AEventResponseAnalysis : public AActor,public PacketAnalysisBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEventResponseAnalysis();
+	virtual void Analysis(BitArrayEnumerator* BitArrayEnumerator) override;
+	
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	TArray<EventPacketAnalysisBase*> EventPacketAnalysisBases;
+};
