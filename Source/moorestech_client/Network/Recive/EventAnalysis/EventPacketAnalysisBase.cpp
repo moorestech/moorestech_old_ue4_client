389561028c70f149c4da1fd2541fb7a364115a9d
sakastudio <sakastudio100@gmail.com>
2021-11-14 22:48:18 +0900

    イベントの基底クラス

---
+++ Source/moorestech_client/Network/Recive/EventAnalysis/EventPacketAnalysisBase.cpp
@@ -0,0 +1,1 @@
+﻿#include "EventPacketAnalysisBase.h"
---
+++ Source/moorestech_client/Network/Recive/EventAnalysis/EventPacketAnalysisBase.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "moorestech_client/Network/Util/BitArrayEnumerator.h"
+
+class EventPacketAnalysisBase
+{
+public:
+	virtual ~EventPacketAnalysisBase() = default;
+	virtual void Analysis(BitArrayEnumerator* BitArrayEnumerator);
+	
+};
