71d49d7ffda4e6546f7d2684d2c12491aba2c1e4
sakastudio <sakastudio100@gmail.com>
2021-11-14 23:36:17 +0900

    クラス作成

---
+++ Source/moorestech_client/Network/Recive/EventAnalysis/BlockPlaceEventAnalysis.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/EventPacketAnalysisBase.h"
+#include "GameFramework/Actor.h"
+#include "BlockPlaceEventAnalysis.generated.h"
+
+UCLASS()
+class MOORESTECH_CLIENT_API ABlockPlaceEventAnalysis : public AEventPacketAnalysisBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Analysis(BitArrayEnumerator* BitArrayEnumerator) override;
+};
