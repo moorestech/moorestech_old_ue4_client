1adab649fd334e07b732d06da5e8384ebe0b37dc
sakastudio <sakastudio100@gmail.com>
2021-11-15 16:33:08 +0900

    クラス定義

--- Source/moorestech_client/GameSystem/World/ChunkData.h
+++ Source/moorestech_client/GameSystem/World/ChunkData.h
@@ -1,28 +1,33 @@
 #pragma once
+#include <iostream>
+#include <vector>
 #include "moorestech_client/GameSystem/Blocks/Base/moorestechBlockBase.h"
 
 class ChunkData
 {
 public:
-	ChunkData(int X, int Y, TArray<int> BlockIds, TArray<AMoorestechBlockBase*>& BlockBases)
+	ChunkData(int X, int Y)
 		: X(X),
-		  Y(Y),
-		  blockIds(BlockIds),
-		  blockBases(BlockBases)
+		  Y(Y)
 	{
 	}
-
+	~ChunkData();
+	
 	int GetX();
 	int GetY();
+	
 	int GetBlockId(int index);
 	void SetBlockId(int index,int id);
+	void AddBlockId(int id);
+	
 	AMoorestechBlockBase& GetBlock(int index);
 	void SetBlock(int index,AMoorestechBlockBase* block);
+	void AddBlock(AMoorestechBlockBase* block);
 	
 	
 private:
 	int X;
 	int Y;
-	TArray<int> blockIds;
-	TArray<AMoorestechBlockBase*>& blockBases;
+	std::vector<int> blockIds;
+	std::vector<AMoorestechBlockBase*> blockBases;
 };
