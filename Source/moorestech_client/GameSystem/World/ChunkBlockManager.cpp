1de1a9ab30f7d456f7ab140685418cf39162400e
sakastudio <sakastudio100@gmail.com>
2021-11-16 14:58:51 +0900

    indexの計算式

--- Source/moorestech_client/GameSystem/World/ChunkBlockManager.cpp
+++ Source/moorestech_client/GameSystem/World/ChunkBlockManager.cpp
@@ -62,13 +62,25 @@ void AChunkBlockManager::SetBlock(int blockX, int blockY, int blockId)
         }
     }
 
+	int BlockXinChunk = 0;
+	int BlockYinChunk= 0;
+	//ブロックの座標からチャンク内の座標を求める
+	if(chunkX < 0)
+	{
+		BlockXinChunk = blockX + -chunkX;
+	}else
+	{
+		BlockXinChunk = blockX - chunkX;
+	}
+	if (chunkY < 0)
+	{
+		BlockYinChunk = blockY + -chunkY;
+	}else{
+		BlockYinChunk = blockY - chunkY;
+	}
 	
-	int blockIndex = (blockY % ChunkSize) + (blockY % ChunkSize) * ChunkSize;
-	//indexを絶対値に
-	if (blockIndex < 0)
-    {
-        blockIndex = -blockIndex;
-    }
+	int blockIndex = BlockYinChunk + BlockXinChunk * ChunkSize;
+
 	UE_LOG(LogTemp, Warning, TEXT("ChunkX:%d ChunkY:%d BlockX:%d BlockY:%d BlockIndex:%d"),chunkX, chunkY , blockX, blockY, blockIndex);
 	//sSetBlockEvent(chunkX, chunkY,blockX,blockY, blockIndex, blockId);
 }
