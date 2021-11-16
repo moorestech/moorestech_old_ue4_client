197d84438b103d336a56bab06aca21aeda3fb7ca
sakastudio <sakastudio100@gmail.com>
2021-11-16 14:38:03 +0900

    チャンクの計算方式の変更

--- Source/moorestech_client/GameSystem/World/ChunkBlockManager.cpp
+++ Source/moorestech_client/GameSystem/World/ChunkBlockManager.cpp
@@ -7,18 +7,69 @@
 
 void AChunkBlockManager::SetChunk(int chunkX,int chunky,TArray<int>& chunkData)
 {
-	SetChunkEvent(chunkX,chunky,chunkData);
+	//SetChunkEvent(chunkX,chunky,chunkData);
 }
 
+//BP側で扱いやすいデータに変換する
 void AChunkBlockManager::SetBlock(int blockX, int blockY, int blockId)
 {
-	//BP側で扱いやすいデータに変換する
 	int ChunkSize = CHUNK_SIZE;
+
+	//C++は余剰の計算がちゃんとしてないのでif文でチャンク座標を求める
+	int chunkX = 0;
+	if(blockX < 0)
+	{
+		if(-blockX < ChunkSize)
+		{
+			chunkX = -ChunkSize;
+		}
+		else
+		{
+			chunkX = blockX - ChunkSize - (ChunkSize + blockX);
+		}
+	}else
+	{
+		if(blockX < ChunkSize)
+		{
+			chunkX = 0;
+		}
+		else
+		{
+			chunkX = blockX - (blockX % ChunkSize);
+		}
+	}
 	
-	int chunkX = blockX -  (blockX % ChunkSize);
-	int chunkY = blockY -  (blockY % ChunkSize);
-	int blockIndex = (blockY % ChunkSize) + (blockX - chunkX) * ChunkSize;
+	int chunkY = 0;
+	if(blockY < 0)
+	{
+		if(-blockY < ChunkSize)
+		{
+			chunkY = -ChunkSize;
+		}
+		else
+		{
+			chunkY = blockY - ChunkSize - (ChunkSize + blockY);
+		}
+	}else
+    {
+        if(blockY < ChunkSize)
+        {
+            chunkY = 0;
+        }
+        else
+        {
+            chunkY = blockY - (blockY % ChunkSize);
+        }
+    }
+
 	
-	SetBlockEvent(chunkX, chunkY,blockX,blockY, blockIndex, blockId);
+	int blockIndex = (blockY % ChunkSize) + (blockY % ChunkSize) * ChunkSize;
+	//indexを絶対値に
+	if (blockIndex < 0)
+    {
+        blockIndex = -blockIndex;
+    }
+	UE_LOG(LogTemp, Warning, TEXT("ChunkX:%d ChunkY:%d BlockX:%d BlockY:%d BlockIndex:%d"),chunkX, chunkY , blockX, blockY, blockIndex);
+	//sSetBlockEvent(chunkX, chunkY,blockX,blockY, blockIndex, blockId);
 }
 
