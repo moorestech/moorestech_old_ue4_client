db18debab4c0c31db7f7d3e24b7a8275503d1843
sakastudio <sakastudio100@gmail.com>
2021-11-18 20:43:45 +0900

    チャンク原点の計算方法変更

--- Source/moorestech_client/GameSystem/World/BlockPositionToChunkPosition.cpp
+++ Source/moorestech_client/GameSystem/World/BlockPositionToChunkPosition.cpp
@@ -7,53 +7,20 @@
 FVector UBlockPositionToChunkPosition::BlockPositionToChunkPosition(int blockX, int blockY)
 {
 	int ChunkSize = CHUNK_SIZE;
-
-	//C++は余剰の計算がちゃんとしてないのでif文でチャンク座標を求める
-	int chunkX = 0;
-	if(blockX < 0)
-	{
-		if(-blockX < ChunkSize)
-		{
-			chunkX = -ChunkSize;
-		}
-		else
-		{
-			chunkX = blockX - ChunkSize - (ChunkSize + blockX);
-		}
-	}else
-	{
-		if(blockX < ChunkSize)
-		{
-			chunkX = 0;
-		}
-		else
-		{
-			chunkX = blockX - (blockX % ChunkSize);
-		}
-	}
+	int chunkX = blockX / ChunkSize;
 	
-	int chunkY = 0;
-	if(blockY < 0)
+	if (blockX < 0)
 	{
-		if(-blockY < ChunkSize)
-		{
-			chunkY = -ChunkSize;
-		}
-		else
-		{
-			chunkY = blockY - ChunkSize - (ChunkSize + blockY);
-		}
-	}else
-	{
-		if(blockY < ChunkSize)
-		{
-			chunkY = 0;
-		}
-		else
-		{
-			chunkY = blockY - (blockY % ChunkSize);
-		}
+		chunkX -= 1;
 	}
-	FVector r = {(float)chunkX, (float)chunkY, (float)0};
+	int chunkY = blockY / ChunkSize;
+	if (blockY < 0)
+	{
+		chunkY -= 1;
+	}
+	int chunkXposition = chunkX * ChunkSize;
+	int chunkYposition = chunkY * ChunkSize;
+	
+	FVector r = {(float)chunkXposition, (float)chunkYposition, (float)0};
 	return r;
 }
