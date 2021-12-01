a6d7683b2d240dc4c7ed499983b5c6a24541f0b4
sakastudio <sakastudio100@gmail.com>
2021-12-01 21:43:23 +0900

    パケットを受け取るクラス

---
+++ Source/moorestech_client/Network/Recive/PacketAnalysis/PlayerInventoryPacketAnalysis.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/PacketAnalysisBase.h"
+#include "GameFramework/Actor.h"
+#include "moorestech_client/GameSystem/UI/PlayerInventoryController.h"
+#include "PlayerInventoryPacketAnalysis.generated.h"
+
+UCLASS()
+class MOORESTECH_CLIENT_API APlayerInventoryPacketAnalysis : public AActor,public PacketAnalysisBase
+{
+	GENERATED_BODY()
+
+public:
+	APlayerInventoryPacketAnalysis();
+	virtual void Analysis(BitArrayEnumerator* BitArrayEnumerator) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	APlayerInventoryController* PlayerInventoryController;
+	
+	virtual void Tick(float DeltaTime) override;
+};
