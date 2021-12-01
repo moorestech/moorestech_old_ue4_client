d02eb1033171677c5305dbcd31cea0e15fcc3ea2
sakastudio <sakastudio100@gmail.com>
2021-12-02 00:15:04 +0900

    プレイヤーインベントリのスクリプト

---
+++ Source/moorestech_client/Network/Recive/EventAnalysis/PlayerInventoryUpdateEvent.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerInventoryUpdateEvent.h"
+
+void APlayerInventoryUpdateEvent::Analysis(BitArrayEnumerator* BitArrayEnumerator)
+{
+	int slot = BitArrayEnumerator->MoveNextToInt();
+	int itemID = BitArrayEnumerator->MoveNextToInt();
+	int itemCount = BitArrayEnumerator->MoveNextToInt();
+	PlayerInventoryController->SetInventorySlotEvent(slot, itemID, itemCount);
+}
---
+++ Source/moorestech_client/Network/Recive/EventAnalysis/PlayerInventoryUpdateEvent.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/EventPacketAnalysisBase.h"
+#include "GameFramework/Actor.h"
+#include "moorestech_client/GameSystem/UI/PlayerInventoryController.h"
+#include "PlayerInventoryUpdateEvent.generated.h"
+
+UCLASS()
+class MOORESTECH_CLIENT_API APlayerInventoryUpdateEvent : public AEventPacketAnalysisBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Analysis(BitArrayEnumerator* BitArrayEnumerator) override;
+private:
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	APlayerInventoryController* PlayerInventoryController;
+};
