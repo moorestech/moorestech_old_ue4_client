0eef6ffbec331b506622da6c4c40b2669e223401
sakastudio <sakastudio100@gmail.com>
2021-12-02 12:44:58 +0900

    ヘッダ修正

--- Source/moorestech_client/GameSystem/World/BlockPositionToChunkPosition.cpp
+++ Source/moorestech_client/GameSystem/World/BlockPositionToChunkPosition.cpp
@@ -1,8 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "ChunkBlockManager.h"
 #include "BlockPositionToChunkPosition.h"
+#include "ChunkBlockManager.h"
 
 FVector UBlockPositionToChunkPosition::BlockPositionToChunkPosition(int blockX, int blockY)
 {
